886251d7f4f14780fcc8a2f61a02b19c5f7e3dd0
Alisson <alisson.krul@tuntscorp.com>
2018-10-11 01:15:00 -0300

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,54 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
-import { BarcodeScanner } from '@ionic-native/barcode-scanner';
-import { Toast } from '@ionic-native/toast';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {ErrorHandler, NgModule} from '@angular/core';
+import {IonicApp, IonicErrorHandler, IonicModule} from 'ionic-angular';
+import {SplashScreen} from '@ionic-native/splash-screen';
+import {StatusBar} from '@ionic-native/status-bar';
+import {BarcodeScanner} from '@ionic-native/barcode-scanner';
+import {Toast} from '@ionic-native/toast';
+import {HttpModule} from '@angular/http';
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
-import { DataServiceProvider } from '../providers/data-service/data-service';
+import {MyApp} from './app.component';
+import {HomePage} from '../pages/home/home';
+import {ListPurchasePage} from '../pages/list-purchase/list-purchase';
+import {DataServiceProvider} from '../providers/data-service/data-service';
+import {PurchaseProvider} from '../providers/purchase/purchase';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import {EditPurchasePage} from "../pages/edit-purchase/edit-purchase";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDmW53c_GHD5Kj_Fw8zE6oZw22oWoc5bv8",
+  authDomain: "tidevo-1dd03.firebaseapp.com",
+  databaseURL: "https://tidevo-1dd03.firebaseio.com",
+  projectId: "tidevo-1dd03",
+  storageBucket: "tidevo-1dd03.appspot.com",
+  messagingSenderId: "125946645217"
+}
+
+
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    ListPurchasePage,
+    EditPurchasePage
   ],
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule,
     HttpModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    ListPurchasePage,
+    EditPurchasePage
   ],
   providers: [
     StatusBar,
@@ -32,7 +56,9 @@ import { DataServiceProvider } from '../providers/data-service/data-service';
     {provide: ErrorHandler, useClass: IonicErrorHandler},
     BarcodeScanner,
     Toast,
-    DataServiceProvider
+    DataServiceProvider, //Old
+    PurchaseProvider
   ]
 })
-export class AppModule {}
+export class AppModule {
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,8 +1,13 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-import { BarcodeScanner } from '@ionic-native/barcode-scanner';
-import { Toast } from '@ionic-native/toast';
-import { DataServiceProvider } from '../../providers/data-service/data-service';
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import {BarcodeScanner} from '@ionic-native/barcode-scanner';
+import {Toast} from '@ionic-native/toast';
+import {DataServiceProvider} from '../../providers/data-service/data-service';
+import {Observable} from "rxjs/Observable";
+import {PurchaseProvider} from "../../providers/purchase/purchase";
+import {ListPurchasePage} from "../list-purchase/list-purchase";
+
+//import {ListPurchasePage} from "../list-purchase/list-purchase";
 
 @Component({
   selector: 'page-home',
@@ -12,35 +17,35 @@ export class HomePage {
 
   products: any[] = [];
   selectedProduct: any;
-  productFound:boolean = false;
+  productFound: boolean = false;
+  purchases: Observable<any>;
 
   constructor(public navCtrl: NavController,
-    private barcodeScanner: BarcodeScanner,
-    private toast: Toast,
-    public dataService: DataServiceProvider) {
-      this.dataService.getProducts()
-        .subscribe((response)=> {
-            this.products = response
-            console.log(this.products);
-        });
+              private barcodeScanner: BarcodeScanner,
+              private toast: Toast,
+              public dataService: DataServiceProvider,
+              private purchaseProvider: PurchaseProvider) {
+
+  }
+  ionViewDidLoad() {
+    //this.purchases =  this.purchaseProvider.getAll().valueChanges();
+  }
+
+  public openPurchaseList(){
+    this.navCtrl.push(ListPurchasePage);
   }
 
   scan() {
     this.selectedProduct = {};
     this.barcodeScanner.scan().then((barcodeData) => {
-      this.selectedProduct = this.products.find(product => product.plu === barcodeData.text);
-      if(this.selectedProduct !== undefined) {
-        this.productFound = true;
-        console.log(this.selectedProduct);
-      } else {
-        this.selectedProduct = {};
-        this.productFound = false;
-        this.toast.show('Product not found', '5000', 'center').subscribe(
-          toast => {
-            console.log(toast);
-          }
-        );
-      }
+
+      this.toast.show(`This is the fucking url bitch: ${JSON.stringify(barcodeData)}`, '5000', 'center').subscribe(
+        toast => {
+          console.log(toast);
+        }
+      );
+
+
     }, (err) => {
       this.toast.show(err, '5000', 'center').subscribe(
         toast => {
